7da31b2120fed50bab3a7e11cae819c7084d0ce7
harshad <Harshadkajale333@gmail.com>
2024-02-03 13:06:18 +0530

    commit all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import Home from './Pages/Home/Home'
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Home/>
+    </>
   )
 }
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './Hero'
+import Footer from '../../Components/Footer/Footer'
+import Navbar from '../../Components/Navbar/Navbar'
+
+function Home() {
+  return (
+      <div>
+          <Navbar/>
+          <Hero />
+      <Footer/></div>
+  )
+}
+
+export default Home
