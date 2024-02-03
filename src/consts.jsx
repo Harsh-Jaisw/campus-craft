34637bcc67ddb66898da7f038fb74384b3ca9a5e
Harsh <harshjaiswal28555@gmail.com>
2024-02-03 13:45:08 +0530

    git ignore and const file add

---
+++ src/consts.jsx
@@ -0,0 +1,166 @@
+export const CollegeData=[
+    {
+    CollegeName:"Axis college",details:[
+        {
+            programType:"PG",CourseDuration:"2 Year",CourseName:"Digital Electronics and Communication Engineering",intake:"18",lateral:"",eligibility:"Passed Bachelor's Degree or equivalent in the relevant field. Obtained at least 50% marks (45% marks in case of candidates belonging to reserved category) in the qualifying Examination."
+        },
+        {
+            programType:"PG",CourseDuration:"2 Year",CourseName:"CAD/CAM",intake:"18",lateral:"",eligibility:"Passed Bachelor's Degree or equivalent in the relevant field. Obtained at least 50% marks (45% marks in case of candidates belonging to reserved category) in the qualifying Examination."
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"CE",intake:"60",lateral:"6",eligibility:`Passed 10+2 examination with Physics and Mathematics as compulsory subjects along with one of no Chemistry Biotechnology Biology/ Technical Vocational sutject Computer
+            Science) Information Technology Informatics Practices Agriculture/ Engineering Graphics Business Studies Obtained at least 45% marks (40% marks in case of candidates belonging to reserved category) in the above subjects taken together.
+            OR
+            Passed Diploma in Engineering and Technology) examination with at least 45% marks (40% marks in case of candidates belonging to reserved category subject to vacancies in the First Year, in case the vacancies at lateral entry are exhausted`
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"CS",intake:"120",lateral:"12",eligibility:`Passed 10+2 examination with Physics and Mathematics as compulsory subjects along with one of no Chemistry Biotechnology Biology/ Technical Vocational sutject Computer
+            Science) Information Technology Informatics Practices Agriculture/ Engineering Graphics Business Studies Obtained at least 45% marks (40% marks in case of candidates belonging to reserved category) in the above subjects taken together.
+            OR
+            Passed Diploma in Engineering and Technology) examination with at least 45% marks (40% marks in case of candidates belonging to reserved category subject to vacancies in the First Year, in case the vacancies at lateral entry are exhausted`
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"CS (Data Science)",intake:"30",lateral:"3",eligibility:`Passed 10+2 examination with Physics and Mathematics as compulsory subjects along with one of no Chemistry Biotechnology Biology/ Technical Vocational sutject Computer
+            Science) Information Technology Informatics Practices Agriculture/ Engineering Graphics Business Studies Obtained at least 45% marks (40% marks in case of candidates belonging to reserved category) in the above subjects taken together.
+            OR
+            Passed Diploma in Engineering and Technology) examination with at least 45% marks (40% marks in case of candidates belonging to reserved category subject to vacancies in the First Year, in case the vacancies at lateral entry are exhausted`
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"CS (Aritificial Intelligence & Machine Learning)",intake:"30",lateral:"3",eligibility:`Passed 10+2 examination with Physics and Mathematics as compulsory subjects along with one of no Chemistry Biotechnology Biology/ Technical Vocational sutject Computer
+            Science) Information Technology Informatics Practices Agriculture/ Engineering Graphics Business Studies Obtained at least 45% marks (40% marks in case of candidates belonging to reserved category) in the above subjects taken together.
+            OR
+            Passed Diploma in Engineering and Technology) examination with at least 45% marks (40% marks in case of candidates belonging to reserved category subject to vacancies in the First Year, in case the vacancies at lateral entry are exhausted`
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"EC",intake:"60",lateral:"6",eligibility:`Passed 10+2 examination with Physics and Mathematics as compulsory subjects along with one of no Chemistry Biotechnology Biology/ Technical Vocational sutject Computer
+            Science) Information Technology Informatics Practices Agriculture/ Engineering Graphics Business Studies Obtained at least 45% marks (40% marks in case of candidates belonging to reserved category) in the above subjects taken together.
+            OR
+            Passed Diploma in Engineering and Technology) examination with at least 45% marks (40% marks in case of candidates belonging to reserved category subject to vacancies in the First Year, in case the vacancies at lateral entry are exhausted`
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"EE",intake:"30",lateral:"3",eligibility:`Passed 10+2 examination with Physics and Mathematics as compulsory subjects along with one of no Chemistry Biotechnology Biology/ Technical Vocational sutject Computer
+            Science) Information Technology Informatics Practices Agriculture/ Engineering Graphics Business Studies Obtained at least 45% marks (40% marks in case of candidates belonging to reserved category) in the above subjects taken together.
+            OR
+            Passed Diploma in Engineering and Technology) examination with at least 45% marks (40% marks in case of candidates belonging to reserved category subject to vacancies in the First Year, in case the vacancies at lateral entry are exhausted`
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"IT",intake:"60",lateral:"6",eligibility:`Passed 10+2 examination with Physics and Mathematics as compulsory subjects along with one of no Chemistry Biotechnology Biology/ Technical Vocational sutject Computer
+            Science) Information Technology Informatics Practices Agriculture/ Engineering Graphics Business Studies Obtained at least 45% marks (40% marks in case of candidates belonging to reserved category) in the above subjects taken together.
+            OR
+            Passed Diploma in Engineering and Technology) examination with at least 45% marks (40% marks in case of candidates belonging to reserved category subject to vacancies in the First Year, in case the vacancies at lateral entry are exhausted`
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"ME",intake:"90",lateral:"9",eligibility:`Passed 10+2 examination with Physics and Mathematics as compulsory subjects along with one of no Chemistry Biotechnology Biology/ Technical Vocational sutject Computer
+            Science) Information Technology Informatics Practices Agriculture/ Engineering Graphics Business Studies Obtained at least 45% marks (40% marks in case of candidates belonging to reserved category) in the above subjects taken together.
+            OR
+            Passed Diploma in Engineering and Technology) examination with at least 45% marks (40% marks in case of candidates belonging to reserved category subject to vacancies in the First Year, in case the vacancies at lateral entry are exhausted`
+        },
+        {
+            programType:"Diploma",CourseDuration:"3 Year",CourseName:"EE",intake:"60",lateral:"6",eligibility:`Passed 10th Std./SSC examination, Obtained at least 35% marks in the qualifying examination`
+        },
+        {
+            programType:"Diploma",CourseDuration:"3 Year",CourseName:"ME(Production)",intake:"60",lateral:"6",eligibility:`Passed 10th Std./SSC examination, Obtained at least 35% marks in the qualifying examination`
+        }, {
+            programType:"Diploma",CourseDuration:"3 Year",CourseName:"ME(Automobile)",intake:"60",lateral:"6",eligibility:`Passed 10th Std./SSC examination, Obtained at least 35% marks in the qualifying examination`
+        }, {
+            programType:"Diploma",CourseDuration:"3 Year",CourseName:"ME(Maintenance)",intake:"60",lateral:"6",eligibility:`Passed 10th Std./SSC examination, Obtained at least 35% marks in the qualifying examination`
+        },
+        {
+            programType:"Diploma",CourseDuration:"3 Year",CourseName:"CE",intake:"60",lateral:"6",eligibility:`Passed 10th Std./SSC examination, Obtained at least 35% marks in the qualifying examination`
+        },
+        {
+            programType:"PG",CourseDuration:"2 Year",CourseName:"Fashion Technology",intake:"30",lateral:"",eligibility:"Passed Bachelor Degree in Fine Arts or equivalent Degree. Obtained at least 50% marks (45% marks in case of candidates belonging to reserved category) in the qualifying Examination."
+        },
+        {
+            programType:"PG",CourseDuration:"2 Year",CourseName:"Fine Art(Photography and Media Communication)",intake:"30",lateral:"",eligibility:"Passed Bachelor Degree in Fine Arts or equivalent Degree. Obtained at least 50% marks (45% marks in case of candidates belonging to reserved category) in the qualifying Examination."
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"B.Fad",intake:"60",lateral:"",eligibility:`Passed 10+2 examination.
+            Obtained at least 45% marks (40% marks in case of candidates belonging to reserved category) in the qualifying Examination`
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"BFA",intake:"30",lateral:"",eligibility:`Passed 10+2 examination.
+            Obtained at least 45% marks (40% marks in case of candidates belonging to reserved category) in the qualifying Examination`
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"Film Editing",intake:"30",lateral:"",eligibility:`Passed 10+2 examination.
+            Obtained at least 45% marks (40% marks in case of candidates belonging to reserved category) in the qualifying Examination`
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"Visual Effects",intake:"30",lateral:"",eligibility:`Passed 10+2 examination.
+            Obtained at least 45% marks (40% marks in case of candidates belonging to reserved category) in the qualifying Examination`
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"Cinematography",intake:"30",lateral:"",eligibility:`Passed 10+2 examination.
+            Obtained at least 45% marks (40% marks in case of candidates belonging to reserved category) in the qualifying Examination`
+        },
+        {
+            programType:"Diploma",CourseDuration:"3 Year",CourseName:"Textile Design",intake:"60",lateral:"6",eligibility:`Passed 10th Std./SSC examination, Obtained at least 35% marks in the qualifying examination`
+        },
+        {
+            programType:"Diploma",CourseDuration:"3 Year",CourseName:"Fashion Design and Garment Technology",intake:"60",lateral:"6",eligibility:`Passed 10th Std./SSC examination, Obtained at least 35% marks in the qualifying examination`
+        },
+        {
+            programType:"UG",CourseDuration:"5 Year",CourseName:"B.Arch.",intake:"40",lateral:"",eligibility:`Passed 10+2 examination with Physics, Chemistry and Mathematics as mandatory subjects with 50% marks in aggregate and also at least 50% marks in aggregate of the 10+2 examination.
+            OR
+            Passed 10+3 Diploma examination with Mathematics as compulsory subject having obtained at least 50% marks (45% marks in case of candidates belonging to reserved category) in the aggregate`
+        },
+        {
+            programType:"Diploma",CourseDuration:"3 Year",CourseName:"Interior Design",intake:"60",lateral:"6",eligibility:`Passed 10th Std./SSC examination, Obtained at least 35% marks in the qualifying examination`
+        },
+        {
+            programType:"PG",CourseDuration:"2 Year",CourseName:"MCA",intake:"120",lateral:"",eligibility:`Passed BCA/ Bachelor Degree in Computer Science Engineering or equivalent Degree.
+            OR
+            Passed B.Sc./B.Com./B.A. with Mathematics at 10+2 Level or Graduation Level (with additional bridge Courses as per the norms of the concerned University).
+            Obtained at least 50% marks (45% marks in case of candidates belonging to reserved category) in the qualifying Examination.`
+        },
+        {
+            programType:"PG",CourseDuration:"2 Year",CourseName:"MBA",intake:"120",lateral:"",eligibility:`Passed Bachelor Degree of minimum 3 years duration.
+            Obtained at least 50% marks (45% marks in case of candidates belonging to reserved category) in the qualifying Examination.`
+        },
+        {
+            programType:"UG",CourseDuration:"3 Year",CourseName:"BBA",intake:"180",lateral:"",eligibility:`Must have passed Intermediate (10+2)/Equivalent examination in Science/Arts/Commerce group with minimum 45% marks.`
+        },
+        {
+            programType:"UG",CourseDuration:"3 Year",CourseName:"BCA",intake:"180",lateral:"",eligibility:`Class 12 with Mathematics as a compulsory subject and a minimum of 45% aggregate.`
+        },
+        {
+            programType:"UG",CourseDuration:"3 Year",CourseName:"B.Com",intake:"120",lateral:"",eligibility:`Intermediate of Equivalent`
+        },
+        {
+            programType:"UG",CourseDuration:"3 Year",CourseName:"B.Sc",intake:"240",lateral:"",eligibility:`Intermediate of Equivalent`
+        },
+        {
+            programType:"UG",CourseDuration:"3 Year",CourseName:"B.sc.(Biotechnology)",intake:"60",lateral:"",eligibility:`Intermediate or 10+2 in Science (Zoology, Botany, Chemistry) Examination passed with minimum aggregate of 50% marks. (45% for SC/ST)`
+        },
+        {
+            programType:"UG",CourseDuration:"3 Year",CourseName:"B.sc.(Medical Microbiology)",intake:"60",lateral:"",eligibility:`Intermediate or 10+2 in Science (Zoology, Botany, Chemistry) Examination passed with minimum aggregate of 50% marks. (45% for SC/ST)`
+        },
+        {
+            programType:"UG",CourseDuration:"4 Year",CourseName:"B.Pharm",intake:"60",lateral:"6",eligibility:`Passed 10+2 examination with Physics and Chemistry as compulsory subjects along with Mathematics/ Biology subject.`
+        }, {
+            programType:"Diploma",CourseDuration:"2 Year",CourseName:"D.Pharm",intake:"60",lateral:"",eligibility:`Passed 10+2 examination with Physics and Chemistry as compulsory subjects along with Mathematics/ Biology subject.`
+        },
+    ]
+},
+{
+    CollegeName:"Harsh college",details:[]
+},
+{
+    CollegeName:"Harsh college",details:[]
+},
+{
+    CollegeName:"Harsh college",details:[]
+},
+{
+    CollegeName:"Harsh college",details:[]
+},
+{
+    CollegeName:"Harsh college",details:[]
+},
+{
+    CollegeName:"Harsh college",details:[]
+}
+
+
+]
