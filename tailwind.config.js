83e5dc26a669d10b85208651cffa2e3ed22f4641
sanjay <Bhavishm009@gmail.com>
2024-02-04 23:36:45 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-export default withMT({
+
+export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -19,4 +17,4 @@ export default withMT({
   },
 
   plugins: [require("daisyui")],
-});
+};
