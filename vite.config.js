a2231d5e8232ed4a520e9cdc6fd609b3a26f0bca
sanjay <Bhavishm009@gmail.com>
2024-02-14 19:17:34 +0530

    changes in data

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  proxy: {
+    '/api': 'http://api.hdfonline.in/api/v1/',
+  },
 })
