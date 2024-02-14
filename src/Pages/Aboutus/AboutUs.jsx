c9cef14034e79e392e865433074f2ed2f0578117
sanjay <Bhavishm009@gmail.com>
2024-02-14 19:19:08 +0530

    changes in data

--- src/Pages/Aboutus/AboutUs.jsx
+++ src/Pages/Aboutus/AboutUs.jsx
@@ -6,6 +6,20 @@ import { teamData } from "../../consts";
 
 function AboutUs() {
   const navigate = useNavigate();
+  useEffect(() => {
+    const visitData = {
+      company_id: "1",
+      page_visited: "About",
+    };
+
+    commonApiFunction("/addVisits", "POST", visitData)
+      .then((data) => {
+        console.log("POST request result:", data);
+      })
+      .catch((error) => {
+        console.error("POST request error:", error);
+      });
+  }, []);
 
   return (
     <div>
